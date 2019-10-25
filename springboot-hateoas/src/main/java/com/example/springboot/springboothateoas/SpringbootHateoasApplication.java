2b40855d185079f4a191c6cdbdbc1b9d8e3aeb17
马增群 <mazq@minstone.com.cn>
2019-10-25 15:35:29 +0800

    feat：SpringBoot HATEOAS

---
+++ springboot-hateoas/src/main/java/com/example/springboot/springboothateoas/SpringbootHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.springboothateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHateoasApplication.class, args);
+	}
+
+}
