0afd7a855b865ca50064260b294fe3a888e55007
SmileNicky <3623307211@qq.com>
2019-11-09 10:50:07 +0800

    修缮：改下xml文件命名

--- springboot-properties-config/src/main/java/com/example/springboot/properties/SpringbootPropertiesConfigApplication.java
+++ springboot-properties-config/src/main/java/com/example/springboot/properties/SpringbootPropertiesConfigApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@ImportResource(locations = {"classpath:testBean.xml"})
+//@ImportResource(locations = {"classpath:beans.xml"})
 public class SpringbootPropertiesConfigApplication {
 
 	public static void main(String[] args) {
