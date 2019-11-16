ada92d0ad29203cf94f1af9add83cd59d6b66e30
SmileNicky <3623307211@qq.com>
2019-11-16 23:33:29 +0800

    提交测试类

---
+++ springboot-properties-config/src/main/java/com/example/springboot/properties/test/Y.java
@@ -0,0 +1,22 @@
+package com.example.springboot.properties.test;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author nicky
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2019年11月14日  修改内容:
+ * </pre>
+ */
+@Component
+public class Y {
+
+    public Y(){
+        System.out.println("Y class");
+    }
+}
--- springboot-properties-config/src/test/java/com/example/springboot/properties/SpringbootPropertiesConfigApplicationTests.java
+++ springboot-properties-config/src/test/java/com/example/springboot/properties/SpringbootPropertiesConfigApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.springboot.properties;
 
 import com.example.springboot.properties.bean.User;
+import com.example.springboot.properties.config.AppConfig;
+import com.example.springboot.properties.test.X;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class SpringbootPropertiesConfigApplicationTests {
@@ -24,6 +27,12 @@ class SpringbootPropertiesConfigApplicationTests {
 	void contextLoads() {
 		boolean flag =  ioc.containsBean("testService1");
 		System.out.println(flag);
+
+		AnnotationConfigApplicationContext ioc  = new AnnotationConfigApplicationContext();
+		ioc.register(AppConfig.class);
+		ioc.refresh();
+		System.out.println(ioc.getBean(X.class));
+
 	}
 
 }
