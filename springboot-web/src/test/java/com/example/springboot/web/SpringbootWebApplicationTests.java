e908061531aca7221647519cadf1a126df86a52b
SmileNicky <3623307211@qq.com>
2019-11-22 23:35:18 +0800

    添加web工程

---
+++ springboot-web/src/test/java/com/example/springboot/web/SpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
