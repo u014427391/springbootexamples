d55ddb5a8725f1faa53170224497341dabfbc578
SmileNicky <3623307211@qq.com>
2019-11-24 11:17:00 +0800

    修改web工程

--- springboot-web/src/main/java/com/example/springboot/web/controller/LoginController.java
+++ springboot-web/src/main/java/com/example/springboot/web/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,6 +35,11 @@ public class LoginController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
         return modelAndView;
+    }
+
+    @GetMapping(value = {"/changeI18nLanguage"})
+    public ModelAndView changeI18nLanguage(){
+        return new ModelAndView();
     }
 
 }
