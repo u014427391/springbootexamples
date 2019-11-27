6b824f336ba1e1e971178ed63307d49844fda6c3
SmileNicky <3623307211@qq.com>
2019-11-27 19:30:39 +0800

    提交web工程测试代码

--- springboot-web/src/main/java/com/example/springboot/web/SpringbootWebApplication.java
+++ springboot-web/src/main/java/com/example/springboot/web/SpringbootWebApplication.java
@@ -2,7 +2,12 @@ package com.example.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 //@ComponentScan
@@ -13,5 +18,16 @@ public class SpringbootWebApplication {
 		SpringApplication.run(SpringbootWebApplication.class, args);
 	}
 
+	@Bean
+	public ViewResolver testViewResolver(){
+		return new TestViewResolver();
+	}
+
+	protected static class TestViewResolver implements ViewResolver{
+		@Override
+		public View resolveViewName(String viewName, Locale locale) throws Exception {
+			return null;
+		}
+	}
 
 }
