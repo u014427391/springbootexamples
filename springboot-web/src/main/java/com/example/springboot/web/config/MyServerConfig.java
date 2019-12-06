6de1cfca8dd31fcbaa3a96496230403aebe84175
smileNicky <362330721@qq.com>
2019-12-06 17:26:41 +0800

    提交web工程测试代码

--- springboot-web/src/main/java/com/example/springboot/web/config/MyServerConfig.java
+++ springboot-web/src/main/java/com/example/springboot/web/config/MyServerConfig.java
@@ -3,6 +3,8 @@ package com.example.springboot.web.config;
 import com.example.springboot.web.filter.MyFilter;
 import com.example.springboot.web.listener.MyListener;
 import com.example.springboot.web.servlet.MyServlet;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
@@ -10,8 +12,10 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <pre>
@@ -31,12 +35,12 @@ public class MyServerConfig {
      * 自定义的WebServerFactory定制器类
      * @return
      */
-    //@Bean
-    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryWebServerFactoryCustomizer(){
-        return new WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>() {
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> webServerFactoryCustomizer(){
+        return new WebServerFactoryCustomizer<TomcatServletWebServerFactory>() {
             @Override
-            public void customize(ConfigurableServletWebServerFactory factory) {
-                factory.setPort(8081);
+            public void customize(TomcatServletWebServerFactory factory) {
+                factory.setPort(8082);
             }
         };
     }
