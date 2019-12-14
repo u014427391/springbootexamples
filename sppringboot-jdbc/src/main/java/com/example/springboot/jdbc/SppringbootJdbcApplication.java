d02567ef828bdd0c26ac69a88b5069bac82c1a38
smileNicky <362330721@qq.com>
2019-12-14 22:07:52 +0800

    提交jdbc学习例子

---
+++ sppringboot-jdbc/src/main/java/com/example/springboot/jdbc/SppringbootJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SppringbootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SppringbootJdbcApplication.class, args);
+	}
+
+}
---
+++ sppringboot-jdbc/src/test/java/com/example/springboot/jdbc/SppringbootJdbcApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.springboot.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class SppringbootJdbcApplicationTests {
+
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	public void contextLoads() throws SQLException {
+		System.out.println(dataSource.getClass());
+		Connection connection = dataSource.getConnection();
+		System.out.println(connection);
+		connection.close();
+	}
+
+}
