bbc138608be821e2f75c5e4468a1b0cf3dbf73fc
smileNicky <362330721@qq.com>
2020-01-01 16:58:28 +0800

    提交Springboot几个重要监听机制类测试例子

---
+++ springboot-web/src/main/java/com/example/springboot/web/listener/HelloApplicationContextInitializer.java
@@ -0,0 +1,11 @@
+package com.example.springboot.web.listener;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class HelloApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        System.out.println("ApplicationContextInitializer...initialize..."+applicationContext);
+    }
+}
---
+++ springboot-web/src/main/java/com/example/springboot/web/listener/HelloCommandLineRunner.java
@@ -0,0 +1,15 @@
+package com.example.springboot.web.listener;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+
+@Component
+public class HelloCommandLineRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("CommandLineRunner...run..."+ Arrays.asList(args));
+    }
+}
