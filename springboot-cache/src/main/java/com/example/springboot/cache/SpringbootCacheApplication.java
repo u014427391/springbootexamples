1e3d716e4f98b0741f4443cc5ff18b179fc4d402
nicky.ma <mazq@minstone.com.cn>
2020-03-31 16:20:25 +0800

    提交SpringBoot Cache使用例子工程

---
+++ springboot-cache/src/main/java/com/example/springboot/cache/SpringbootCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCacheApplication.class, args);
+    }
+
+}
