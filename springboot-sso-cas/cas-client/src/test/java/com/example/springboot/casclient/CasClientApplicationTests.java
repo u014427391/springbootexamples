091e66fd68730cf7bd8e0fd2fab33128810d6ad3
马增群 <mazq@minstone.com.cn>
2020-04-29 11:21:22 +0800

    SpringBoot集成CAS单点登录离职

---
+++ springboot-sso-cas/cas-client/src/test/java/com/example/springboot/casclient/CasClientApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.springboot.casclient;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasClientApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
