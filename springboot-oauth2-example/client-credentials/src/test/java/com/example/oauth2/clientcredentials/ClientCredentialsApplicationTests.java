541f90f8736678c895c51f29f9b4a4932e0e09be
马增群 <mazq@minstone.com.cn>
2020-06-11 16:53:37 +0800

    提交OAuth2.0客户端模式学习例子

---
+++ springboot-oauth2-example/client-credentials/src/test/java/com/example/oauth2/clientcredentials/ClientCredentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2.clientcredentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCredentialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
