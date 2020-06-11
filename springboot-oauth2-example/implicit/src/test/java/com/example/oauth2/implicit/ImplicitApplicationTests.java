3bbb4ea173de22e46d8c7ce600f1499994a9ee9c
马增群 <mazq@minstone.com.cn>
2020-06-11 14:16:11 +0800

    提交OAuth2.0简化模式测试代码

---
+++ springboot-oauth2-example/implicit/src/test/java/com/example/oauth2/implicit/ImplicitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.oauth2.implicit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImplicitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
