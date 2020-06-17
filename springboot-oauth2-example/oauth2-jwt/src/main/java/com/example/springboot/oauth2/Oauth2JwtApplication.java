58b6a0fc76cf5b4426fea06b00063a4532dfa276
马增群 <mazq@minstone.com.cn>
2020-06-17 14:42:13 +0800

    JWT方式实现的令牌

---
+++ springboot-oauth2-example/oauth2-jwt/src/main/java/com/example/springboot/oauth2/Oauth2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2JwtApplication.class, args);
+    }
+
+}
