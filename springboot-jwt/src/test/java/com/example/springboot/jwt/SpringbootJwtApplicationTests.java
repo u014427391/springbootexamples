8ad1d1c093407cb6a9c508683591b64d7f253e72
马增群 <mazq@minstone.com.cn>
2020-07-02 15:52:25 +0800

    提交SpringBoot集成jwt例子工程

---
+++ springboot-jwt/src/test/java/com/example/springboot/jwt/SpringbootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
