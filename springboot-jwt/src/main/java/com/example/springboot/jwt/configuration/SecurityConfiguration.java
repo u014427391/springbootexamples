dbcd543245e826e714c83765f9b603dcd99ea516
马增群 <mazq@minstone.com.cn>
2020-07-07 18:04:19 +0800

    JWT代码提交

---
+++ springboot-jwt/src/main/java/com/example/springboot/jwt/configuration/SecurityConfiguration.java
@@ -0,0 +1,85 @@
+package com.example.springboot.jwt.configuration;
+
+
+import com.example.springboot.jwt.component.CustomPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.Resource;
+
+/**
+ * <pre>
+ *  SecurityConfiguration
+ * </pre>
+ *
+ * <pre>
+ * @author mazq
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2020/04/30 15:58  修改内容:
+ * </pre>
+ */
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("jwtUserService")
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new CustomPasswordEncoder());
+        auth.parentAuthenticationManager(authenticationManagerBean());
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //解决静态资源被拦截的问题
+        web.ignoring().antMatchers("/asserts/**");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http   // 配置登录页并允许访问
+                .formLogin().permitAll()
+                // 配置登出页面
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
+                .and().authorizeRequests().antMatchers("/oauth/**", "/login/**", "/logout/**").permitAll()
+                // 其余所有请求全部需要鉴权认证
+                .anyRequest().authenticated()
+                // 关闭跨域保护;
+                .and().csrf().disable();
+    }
+
+
+
+    @Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
