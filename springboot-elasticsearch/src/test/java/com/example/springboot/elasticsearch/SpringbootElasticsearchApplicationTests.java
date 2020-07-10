15f085e9c58f22c221c9e01abb99d8bf411edb75
nicky <mazq@minstone.com.cn>
2020-07-10 16:59:39 +0800

    elasticsearch例子工程提交

---
+++ springboot-elasticsearch/src/test/java/com/example/springboot/elasticsearch/SpringbootElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
