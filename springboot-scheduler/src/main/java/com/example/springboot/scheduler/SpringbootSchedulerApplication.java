736e10c3b22b872c5bfbda2926c0277b78f2d6f2
nicky <mazq@minstone.com.cn>
2020-07-20 17:34:45 +0800

    SpringBoot集成Spring task例子工程

---
+++ springboot-scheduler/src/main/java/com/example/springboot/scheduler/SpringbootSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSchedulerApplication.class, args);
+    }
+
+}
---
+++ springboot-scheduler/src/main/java/com/example/springboot/scheduler/configuration/TaskSchedulerConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.springboot.scheduler.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+/**
+ * <pre>
+ *      TaskSchedulerConfiguration
+ * </pre>
+ *
+ * <pre>
+ * @author mazq
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2020/07/20 13:57  修改内容:
+ * </pre>
+ */
+@Configuration
+@EnableScheduling
+public class TaskSchedulerConfiguration {
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5);
+        threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
+
+}
