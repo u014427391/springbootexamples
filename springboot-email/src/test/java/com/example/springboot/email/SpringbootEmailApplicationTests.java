c56f3df007d5b03488d6e3e6455bd23fa62151c3
nicky <mazq@minstone.com.cn>
2020-07-22 16:11:31 +0800

    SpringBoot集成Email发送邮件

---
+++ springboot-email/src/test/java/com/example/springboot/email/SpringbootEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
