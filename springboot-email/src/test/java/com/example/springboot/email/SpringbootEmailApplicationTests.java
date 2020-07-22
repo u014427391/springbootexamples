30a9b8878a6fe937430284c9b062f74d238408bb
nicky <mazq@minstone.com.cn>
2020-07-22 17:24:03 +0800

    测试邮件发送

--- springboot-email/src/test/java/com/example/springboot/email/SpringbootEmailApplicationTests.java
+++ springboot-email/src/test/java/com/example/springboot/email/SpringbootEmailApplicationTests.java
@@ -20,7 +20,7 @@ class SpringbootEmailApplicationTests {
     @Test
     void testSendTextMail(){
         EmailDto emailDto = new EmailDto();
-        emailDto.setSendTo("2284087296@qq.com");
+        emailDto.setSendTo("your_email@qq.com");
         emailDto.setSubject("发送文本邮件");
         emailDto.setContent("测试发送文本邮件!");
         emailService.sendTextMail(emailDto);
@@ -29,7 +29,7 @@ class SpringbootEmailApplicationTests {
     @Test
     void testSendHtmlMail() throws MessagingException {
         EmailDto emailDto = new EmailDto();
-        emailDto.setSendTo("2284087296@qq.com");
+        emailDto.setSendTo("your_email@qq.com");
         emailDto.setSubject("发送html邮件");
         String html = "<html><head><title>email</title></head><body><h1>测试发送html邮件</h1></body></html>";
         emailDto.setContent(html);
@@ -40,7 +40,7 @@ class SpringbootEmailApplicationTests {
     void testSendAttachmentMail() throws MessagingException {
         String[] filePaths = new String[]{"C:\\Users\\Administrator\\Desktop\\awrrpt_2_22022_22023.html","C:\\Users\\Administrator\\Desktop\\awr.html"};
         EmailDto emailDto = new EmailDto();
-        emailDto.setSendTo("2284087296@qq.com");
+        emailDto.setSendTo("your_email@qq.com");
         emailDto.setSubject("发送附件邮件");
         emailDto.setFilePaths(filePaths);
         String html = "<html><head><title>email</title></head><body><h1>测试发送附件邮件</h1></body></html>";
@@ -50,10 +50,10 @@ class SpringbootEmailApplicationTests {
 
     @Test
     void testSendInLineImgMail() throws MessagingException {
-        String inlineImgPath = "C:\\Users\\zhaoh\\Desktop\\007Tv3Vmly1ggxvrzrytij31hc0u047h.jpg";
+        String inlineImgPath = "C:\\Users\\Administrator\\Desktop\\007Tv3Vmly1ggxvrzrytij31hc0u047h.jpg";
         String srcId = "img01";
         EmailDto emailDto = new EmailDto();
-        emailDto.setSendTo("2284087296@qq.com");
+        emailDto.setSendTo("your_email@qq.com");
         emailDto.setSubject("发送html内嵌图片邮件");
         emailDto.setInlineImgId(srcId);
         emailDto.setInlineImgPath(inlineImgPath);
