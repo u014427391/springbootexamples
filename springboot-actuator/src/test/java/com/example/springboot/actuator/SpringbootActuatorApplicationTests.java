f90ab6cd765d153bfc05d386b4c1d68190cd8409
nicky <mazq@minstone.com.cn>
2020-07-23 14:01:08 +0800

    SpringBoot actuator监控管理工程

---
+++ springboot-actuator/src/test/java/com/example/springboot/actuator/SpringbootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
