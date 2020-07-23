ae536a37fc555ad58ea5b0833894cac27c475b5c
nicky <mazq@minstone.com.cn>
2020-07-23 18:03:34 +0800

    SpringBoot actuator监控管理工程

--- springboot-actuator/src/main/java/com/example/springboot/actuator/controller/IndexController.java
+++ springboot-actuator/src/main/java/com/example/springboot/actuator/controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.example.springboot.actuator.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * <pre>
- *
+ *  IndexController
  * </pre>
  *
  * <pre>
@@ -11,5 +16,12 @@ package com.example.springboot.actuator.controller;
  *    修改后版本:     修改人：  修改日期: 2020/07/23 17:53  修改内容:
  * </pre>
  */
+@Controller
 public class IndexController {
+
+    @RequestMapping(value = {"/test"})
+    @ResponseBody
+    public String test(){
+        return "success";
+    }
 }
