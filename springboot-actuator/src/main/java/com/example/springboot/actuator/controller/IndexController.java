e86478c5cca18d48456f63a9894ea44745185e70
nicky <mazq@minstone.com.cn>
2020-07-23 18:01:40 +0800

    SpringBoot actuator监控管理工程

---
+++ springboot-actuator/src/main/java/com/example/springboot/actuator/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.springboot.actuator.controller;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * <pre>
+ * @author mazq
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2020/07/23 17:53  修改内容:
+ * </pre>
+ */
+public class IndexController {
+}
