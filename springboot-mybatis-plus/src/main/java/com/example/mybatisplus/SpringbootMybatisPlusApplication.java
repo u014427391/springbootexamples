b61fd39de9bf97f4ab8d93fbd8b4ae2467dc9eef
SmileNicky <362330721@qq.com>
2021-12-12 15:56:20 +0800

    提交Mybatis plus工程

---
+++ springboot-mybatis-plus/src/main/java/com/example/mybatisplus/SpringbootMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisPlusApplication.class, args);
+    }
+
+}
---
+++ springboot-mybatis-plus/src/test/java/com/example/mybatisplus/SpringbootMybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
