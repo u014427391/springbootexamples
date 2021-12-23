e9851ce3abed615bd2ef41e7494bf046787bd427
nicky <362330721@qq.com>
2021-12-23 09:02:46 +0800

    提交kafka工程

---
+++ springboot-kafka/src/test/java/com/example/kafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
