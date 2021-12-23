19c813311bed23ce1729f70317bf6723a596c3a9
nicky <362330721@qq.com>
2021-12-23 09:03:03 +0800

    提交阿里canal工程

---
+++ springboot-canal/src/main/java/com/example/ali/canal/SpringbootCanalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ali.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCanalApplication.class, args);
+    }
+
+}
---
+++ springboot-canal/src/test/java/com/example/ali/canal/SpringbootCanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ali.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
