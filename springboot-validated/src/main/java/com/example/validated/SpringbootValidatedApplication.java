92d3d2e5db23f4e9e4a9d51ba4e9209435cba800
nicky <362330721@qq.com>
2022-01-04 11:17:08 +0800

    springboot 表单参数校验过程提交

---
+++ springboot-validated/src/main/java/com/example/validated/SpringbootValidatedApplication.java
@@ -0,0 +1,13 @@
+package com.example.validated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootValidatedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootValidatedApplication.class, args);
+    }
+
+}
---
+++ springboot-validated/src/test/java/com/example/validated/SpringbootValidatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootValidatedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
