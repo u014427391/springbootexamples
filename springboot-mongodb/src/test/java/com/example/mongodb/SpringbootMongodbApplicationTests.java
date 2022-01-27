6feb8e2f77f327a5ab8005dfdfa2888ad9acc2cb
nicky <362330721@qq.com>
2022-01-27 16:35:39 +0800

    提交MongoDB工程

---
+++ springboot-mongodb/src/test/java/com/example/mongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
