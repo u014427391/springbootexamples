12a31306f8340f8ff9cbca82bffb7d6341cb1631
mazengq <mazengq@minstone.com.cn>
2022-01-27 18:28:03 +0800

    mongoDB configuration

---
+++ springboot-mongodb/src/main/java/com/example/mongodb/configuration/MongoConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.mongodb.configuration;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+public class MongoConfiguration extends AbstractMongoClientConfiguration {
+
+
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString str = new ConnectionString("mongodb://localhost:27017/test")
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(str)
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "test";
+    }
+}
