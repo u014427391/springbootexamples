b1f976f9fdbd339487d47bd40c55c83c1f6d0302
nicky <362330721@qq.com>
2022-02-08 16:20:29 +0800

    MongoDB聚合例子提交

--- springboot-mongodb/src/test/java/com/example/mongodb/MongodbAggregationTests.java
+++ springboot-mongodb/src/test/java/com/example/mongodb/MongodbAggregationTests.java
@@ -4,22 +4,27 @@ package com.example.mongodb;
 
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.print.Doc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 
-import static com.mongodb.client.model.Aggregates.match;
-import static com.mongodb.client.model.Aggregates.count;
+import static com.mongodb.client.model.Aggregates.*;
 import static org.junit.Assert.assertEquals;
 
 
@@ -35,26 +40,67 @@ public class MongodbAggregationTests {
 
     @BeforeClass
     public static void init() throws IOException {
-        mongoClient = new MongoClient("192.168.0.61", 27017);
+        mongoClient = new MongoClient("127.0.0.1", 27017);
         mongoDatabase = mongoClient.getDatabase(DATABASE);
         collection = mongoDatabase.getCollection(COLLECTION);
 
-        collection.drop();
+        //collection.drop();
 
-        InputStream inputStream = MongodbAggregationTests.class.getResourceAsStream(USER_JSON);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        reader.lines()
-                .forEach(l->collection.insertOne(Document.parse(l)));
-        reader.close();
+//        InputStream inputStream = MongodbAggregationTests.class.getResourceAsStream(USER_JSON);
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//        reader.lines()
+//                .forEach(l->collection.insertOne(Document.parse(l)));
+//        reader.close();
 
     }
 
     @Test
-    public void matchCount() {
-        Document count = collection.aggregate(
+    public void matchCountTest() {
+        Document first = collection.aggregate(
                 Arrays.asList(match(Filters.eq("name", "User1")), count()))
                 .first();
-        assertEquals(9 , count.get("count"));
+        log.info("count:{}" , first.get("count"));
+        assertEquals(1 , first.get("count"));
+    }
+
+
+    @Test
+    public void skipTest() {
+        AggregateIterable<Document> iterable = collection.aggregate(Arrays.asList(skip(5)));
+        for (Document next : iterable) {
+            log.info("user:{}" ,next);
+        }
+
+    }
+
+    @Test
+    public void groupTest() {
+        AggregateIterable<Document> iterable = collection.aggregate(Arrays.asList(
+                group("$name" , Accumulators.sum("nameCount" , 1)),
+                sort(Sorts.ascending("_id"))
+                ));
+        for (Document next : iterable) {
+            log.info("user:{}" ,next);
+        }
+
+    }
+
+    @Test
+    public void sortLimitOutTest() {
+        collection.aggregate(Arrays.asList(
+                sort(Sorts.descending("_id")) ,
+                limit(3) ,
+                out("new_user"))).toCollection();
+
+        MongoCollection collection = mongoDatabase.getCollection("new_user");
+        assertEquals(3 , collection.countDocuments());
+
+        FindIterable<Document> iterable = collection.find();
+        for (Document next : iterable) {
+            log.info("user : {}" , next);
+        }
+
+
     }
 
 
