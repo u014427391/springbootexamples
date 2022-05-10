4f2588eb7348afbb30fe20da639ff3c4da3d7463
nicky <362330721@qq.com>
2022-05-10 16:44:23 +0800

    spring security配置Content Security Policy

---
+++ springboot-security-csp/src/main/java/com/example/security/SpringbootSecurityCspApplication.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityCspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityCspApplication.class, args);
+    }
+
+}
---
+++ springboot-security-csp/src/test/java/com/example/security/SpringbootSecurityCspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityCspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
