abb53499a3dd2311850d2fdb47faea3a3c48a34a
nicky <362330721@qq.com>
2022-05-12 18:01:38 +0800

    加上代码注释

--- springboot-security-csp/src/main/java/com/example/security/configuration/ContentSecurityPolicySecurityConfiguration.java
+++ springboot-security-csp/src/main/java/com/example/security/configuration/ContentSecurityPolicySecurityConfiguration.java
@@ -20,12 +20,20 @@ public class ContentSecurityPolicySecurityConfiguration extends WebSecurityConfi
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // 自定义一个登录页面，需要写一个/login接口来跳转，
+        // usernameParameter指定自定义的login.html页面的input标签用户密码属性
         http.formLogin().usernameParameter("username").passwordParameter("password").loginPage("/login").permitAll()
+            // 关闭跨域保护;
             .and().csrf().disable()
+            // 开放权限，不需要登录也可以访问
             .authorizeRequests().antMatchers("/login/**", "/logout/**","/report/**").permitAll()
+            // 其它的请求都需要登录验证
             .anyRequest().authenticated()
+            // 设置Report-To，发生一些比如CSP拦截，会发送报告到自己定义的report接口
             .and().headers().addHeaderWriter(new StaticHeadersWriter("Report-To", REPORT_TO))
+            // 设置xss防护
             .xssProtection()
+            // 设置CSP内容安全策略
             .and().contentSecurityPolicy("form-action 'self'; report-uri /report; report-to csp-violation-report");
     }
 
