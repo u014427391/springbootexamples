c3f74cad7c76fde09f1a459773002e9e3994cc90
SmileNicky <362330721@qq.com>
2023-05-11 21:37:33 +0800

    代码启动报错修改

--- springboot-mybatis/src/main/java/com/example/springboot/mybatis/common/enumhandler/IEnum.java
+++ springboot-mybatis/src/main/java/com/example/springboot/mybatis/common/enumhandler/IEnum.java
@@ -1,9 +1,10 @@
 package com.example.springboot.mybatis.common.enumhandler;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-//@JsonDeserialize(using = JacksonEnumDeserializer.class)
+@JsonDeserialize(using = JacksonEnumDeserializer.class)
 public interface IEnum {
 
     String getCode();
