5921780d38b7ff685d5d719aedb94759fb98f812
nicky <2284087296@qq.com>
2023-11-03 15:09:23 +0800

    add：新增Springboot集成Redisson例子工程

---
+++ springboot-redisson/src/main/java/com/example/redission/SpringbootRedissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.redission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedissionApplication.class, args);
+    }
+
+}
---
+++ springboot-redisson/src/test/java/com/example/redission/SpringbootRedissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
