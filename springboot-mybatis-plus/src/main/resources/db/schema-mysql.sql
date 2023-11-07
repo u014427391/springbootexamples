bd71d12770719d07f59b83a1fd4707d78c4b2b7d
nicky <2284087296@qq.com>
2023-11-07 13:44:24 +0800

    sql：修改SQL脚本

--- springboot-mybatis-plus/src/main/resources/db/schema-mysql.sql
+++ springboot-mybatis-plus/src/main/resources/db/schema-mysql.sql
@@ -2,9 +2,12 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user
 (
-    id BIGINT(20) NOT NULL COMMENT '主键ID',
-    name VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
-    age INT(11) NULL DEFAULT NULL COMMENT '年龄',
-    email VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱',
+    `id` BIGINT(20) NOT NULL COMMENT '主键ID',
+    `name` VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
+    `age` INT(11) NULL DEFAULT NULL COMMENT '年龄',
+    `email` VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱',
+    `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `is_deleted` INT(2) DEFAULT 0 COMMENT '是否删除标识,0:否,1:是',
     PRIMARY KEY (id)
 );
