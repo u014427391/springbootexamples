b486050c3a1174bf7f77bd1a704a6fbf215dbcce
nicky <362330721@qq.com>
2023-11-22 20:31:02 +0800

    add:加上jedis例子工程

---
+++ springboot-jedis/src/main/java/com/example/jedis/SpringbootJedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.jedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJedisApplication.class, args);
+    }
+
+}
---
+++ springboot-jedis/src/test/java/com/example/jedis/SpringbootJedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
