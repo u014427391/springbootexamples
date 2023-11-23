9a58e800fbeca18987573eedac1bca41107ff3cf
nicky <362330721@qq.com>
2023-11-23 14:20:35 +0800

    add：加上jedis配置

--- springboot-jedis/src/main/java/com/example/jedis/SpringbootJedisApplication.java
+++ springboot-jedis/src/main/java/com/example/jedis/SpringbootJedisApplication.java
@@ -1,13 +1,18 @@
 package com.example.jedis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootApplication
 public class SpringbootJedisApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootJedisApplication.class, args);
     }
+
+
 
 }
---
+++ springboot-jedis/src/main/java/com/example/jedis/configuration/RedisConfiguration.java
@@ -0,0 +1,47 @@
+package com.example.jedis.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+@ConditionalOnClass({GenericObjectPool.class, JedisConnection.class, Jedis.class})
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+        return new JedisPoolConfig();
+    }
+
+    @Bean
+    public JedisPool jedisPool() {
+      return new JedisPool(jedisPoolConfig());
+    }
+
+
+    @Bean
+    public RedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+}
