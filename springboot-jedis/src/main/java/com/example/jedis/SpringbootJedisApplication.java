75d5558472290867c6a46b4b2f713face79ad91b
nicky <362330721@qq.com>
2023-12-07 11:14:21 +0800

    perf:加上Springboot启动完成时间

--- springboot-jedis/src/main/java/com/example/jedis/SpringbootJedisApplication.java
+++ springboot-jedis/src/main/java/com/example/jedis/SpringbootJedisApplication.java
@@ -1,13 +1,17 @@
 package com.example.jedis;
 
+import cn.hutool.core.date.StopWatch;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@Slf4j
 public class SpringbootJedisApplication {
 
     @Resource
@@ -15,7 +19,12 @@ public class SpringbootJedisApplication {
 
 
     public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("springbootJedis");
         SpringApplication.run(SpringbootJedisApplication.class, args);
+        stopWatch.stop();
+        log.info("Springboot项目启动成功时间:{}ms \n", stopWatch.getTotalTimeMillis());
+        log.info(stopWatch.prettyPrint());
     }
 
     @PreDestroy
