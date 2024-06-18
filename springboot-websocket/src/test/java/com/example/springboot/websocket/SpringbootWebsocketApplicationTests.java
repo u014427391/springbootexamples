535ecea5c4bf04ea6712f06453d1bbdbed65c3f6
nicky <362330721@qq.com>
2024-06-18 16:59:53 +0800

    commit：提交websocket工程

---
+++ springboot-websocket/src/test/java/com/example/springboot/websocket/SpringbootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
