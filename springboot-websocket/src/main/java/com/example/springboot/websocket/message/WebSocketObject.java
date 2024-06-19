0a75f5e8641f2034d5eea325bf00ba54ce0a6434
nicky <362330721@qq.com>
2024-06-19 16:38:14 +0800

    commit：提交websocket工程

--- springboot-websocket/src/main/java/com/example/springboot/websocket/message/WebSocketObject.java
+++ springboot-websocket/src/main/java/com/example/springboot/websocket/message/WebSocketObject.java
@@ -1,5 +1,6 @@
 package com.example.springboot.websocket.message;
 
+import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,10 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Slf4j
 public class WebSocketObject {
 
+    private static final String PREFIX = "socketClient=";
+
+    private String socketClientCode;
+
     private static CopyOnWriteArrayList<WebSocketObject> webSocketSet = new CopyOnWriteArrayList<>();
 
     private Session session ;
@@ -22,7 +27,7 @@ public class WebSocketObject {
     public void onOpen(Session session) {
         this.session = session;
         webSocketSet.add(this);
-        log.info("open a webSocket ,size: {}", getSize());
+        log.info("open a webSocket ,online num: {}", getOnlineNum());
     }
 
     @OnClose
@@ -40,8 +45,24 @@ public class WebSocketObject {
     @OnMessage
     public void onMessage(String message, Session session) {
         log.info("receive message from client:{}", message);
+        // 业务实现
+        if (message.startsWith(PREFIX)) {
+            String socketClientCode = message.substring(PREFIX.length());
+            this.setSocketClientCode(socketClientCode);
+            sendMessage(message);
+            printOnlineClientCode();
+        } else {
+            sendMessage(message);
+        }
     }
 
+    /**
+     * 发送消息
+     *
+     * @Date 2024/06/19 16:36
+     * @Param [message]
+     * @return void
+     */
     public void sendMessage(String message) {
         if (!this.session.isOpen()) {
             log.warn("webSocket is close");
@@ -54,21 +75,53 @@ public class WebSocketObject {
         }
     }
 
+    /**
+     * 给客户端发送消息
+     *
+     * @Date 2024/06/19 16:37
+     * @Param [message, socketClientCode]
+     * @return void
+     */
+    public void sendMessageToClient(String message, String socketClientCode) {
+        log.info("send message to client, message:{}, clientCode:{}", message, socketClientCode);
+
+        printOnlineClientCode();
+
+        webSocketSet.stream().forEach(ws -> {
+            if (StrUtil.isNotBlank(socketClientCode) && StrUtil.isNotBlank(ws.getSocketClientCode()) && ws.getSocketClientCode().equals(socketClientCode)) {
+                ws.sendMessage(message);
+            }
+        });
+    }
+
+    /**
+     * 群发消息
+     *
+     * @Date 2024/06/19 16:37
+     * @Param [message]
+     * @return void
+     */
     public void fanoutMessage(String message) {
         webSocketSet.forEach(ws -> {
             ws.sendMessage(message);
         });
     }
 
-    private static synchronized int getSize() {
+    private static synchronized int getOnlineNum() {
         return webSocketSet.size();
     }
 
-    public Session getSession() {
-        return session;
+    private void printOnlineClientCode() {
+        webSocketSet.stream().forEach(ws -> {
+            log.info("webSocket online:{}", ws.getSocketClientCode());
+        });
     }
 
-    public void setSession(Session session) {
-        this.session = session;
+    public String getSocketClientCode() {
+        return socketClientCode;
+    }
+
+    public void setSocketClientCode(String socketClientCode) {
+        this.socketClientCode = socketClientCode;
     }
 }
