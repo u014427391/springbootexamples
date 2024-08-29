085f68ad7919ec4b1a7e12edaf205eceeb8314fd
nicky <362330721@qq.com>
2024-08-29 15:04:52 +0800

    add:新增ddd微服务例子工程

---
+++ springboot-ddd/src/main/java/com/example/ddd/SpringbootDddApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDddApplication.class, args);
+    }
+
+}
---
+++ springboot-ddd/src/test/java/com/example/ddd/SpringbootDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
