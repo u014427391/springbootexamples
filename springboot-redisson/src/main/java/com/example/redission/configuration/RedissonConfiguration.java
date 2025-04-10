3c9d4130eafb3dcbe633bb8f7a474b889b92ce30
nicky <362330721@qq.com>
2025-04-10 22:54:12 +0800

    perf：添加Redisson配置

--- springboot-redisson/src/main/java/com/example/redission/configuration/RedissonConfiguration.java
+++ springboot-redisson/src/main/java/com/example/redission/configuration/RedissonConfiguration.java
@@ -1,8 +1,25 @@
 package com.example.redission.configuration;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.starter.RedissonProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class RedissonConfiguration {
+
+    @Autowired
+    private RedissonProperties redissonProperties;
+
+    @Bean
+    public RedissonClient redissonClient() throws Exception{
+        Config config = Config.fromYAML(redissonProperties.getConfig());
+        Redisson.create(config);
+        return Redisson.create(config);
+    }
 
 }
