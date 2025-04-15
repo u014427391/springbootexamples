8fc4b7739a731a0b9a51667e36af673e1c1708ad
nicky <362330721@qq.com>
2025-04-15 14:32:28 +0800

    add：新增Springboot集成Arthas例子工程

---
+++ springboot-arthas/src/main/java/com/example/arthas/SpringbootArthasApplication.java
@@ -0,0 +1,13 @@
+package com.example.arthas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootArthasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootArthasApplication.class, args);
+    }
+
+}
---
+++ springboot-arthas/src/test/java/com/example/arthas/SpringbootArthasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arthas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootArthasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
