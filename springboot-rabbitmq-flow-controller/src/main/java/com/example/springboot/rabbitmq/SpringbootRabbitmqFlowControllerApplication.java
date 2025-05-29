6ff816cfcb5984449be8fd6786f4dc5d285f7b33
nicky <362330721@qq.com>
2025-05-29 18:00:17 +0800

    feat：提供RabbitMQ Flow Controller例子工程

---
+++ springboot-rabbitmq-flow-controller/src/main/java/com/example/springboot/rabbitmq/SpringbootRabbitmqFlowControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqFlowControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitmqFlowControllerApplication.class, args);
+    }
+
+}
