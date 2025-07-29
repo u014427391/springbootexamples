0c16733882aee9dcc775e0df2bb15df5a14bc109
nicky <362330721@qq.com>
2025-07-29 13:45:25 +0800

    feat：加上EasyExcel大数据量批量导入工程

---
+++ springboot-easyexcel-batch/src/main/java/com/example/easyexcel/SpringbootEasyexcelBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEasyexcelBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEasyexcelBatchApplication.class, args);
+    }
+
+}
---
+++ springboot-easyexcel-batch/src/test/java/com/example/easyexcel/SpringbootEasyexcelBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easyexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEasyexcelBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
