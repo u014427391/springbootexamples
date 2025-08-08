8c313d0634dd2b1a617f9eb929f465af9e0261a7
nicky <362330721@qq.com>
2025-08-08 18:00:15 +0800

    提交SpringBoot实现Kafka事务例子工程

---
+++ springboot-kafka-transaction/src/main/java/com/example/springbootkafkatransaction/SpringbootKafkaTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkatransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaTransactionApplication.class, args);
+    }
+
+}
---
+++ springboot-kafka-transaction/src/test/java/com/example/springbootkafkatransaction/SpringbootKafkaTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkatransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
