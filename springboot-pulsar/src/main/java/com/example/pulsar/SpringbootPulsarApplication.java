ffdfa12cfee0d386b3149a83420549a5f7cad5d2
nicky <362330721@qq.com>
2025-12-30 16:07:14 +0800

    commit，Springboot集成Pulsar工程

---
+++ springboot-pulsar/src/main/java/com/example/pulsar/SpringbootPulsarApplication.java
@@ -0,0 +1,13 @@
+package com.example.pulsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPulsarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPulsarApplication.class, args);
+    }
+
+}
---
+++ springboot-pulsar/src/test/java/com/example/pulsar/SpringbootPulsarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pulsar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPulsarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
