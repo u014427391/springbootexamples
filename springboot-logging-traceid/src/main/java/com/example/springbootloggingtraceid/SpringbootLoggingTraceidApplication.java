20b9b9d9413e198cd9f2cdc59711c5b92feab607
nicky <362330721@qq.com>
2026-01-23 14:34:53 +0800

    commit：提交Springboot实现日志打印加上traceId demo

---
+++ springboot-logging-traceid/src/main/java/com/example/springbootloggingtraceid/SpringbootLoggingTraceidApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootloggingtraceid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLoggingTraceidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLoggingTraceidApplication.class, args);
+    }
+
+}
