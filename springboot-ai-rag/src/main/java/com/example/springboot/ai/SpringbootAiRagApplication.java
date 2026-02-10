71c217bd731a6352bd2f8e3dd0c9203c473738e3
nicky <362330721@qq.com>
2026-02-10 14:00:56 +0800

    commit：提交ai RAG例子工程

---
+++ springboot-ai-rag/src/main/java/com/example/springboot/ai/SpringbootAiRagApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAiRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAiRagApplication.class, args);
+    }
+
+}
