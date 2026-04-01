a1d4bb795d8b8d7e9111f5d5d529ffe72dcbc193
nicky <362330721@qq.com>
2026-04-01 16:35:56 +0800

    提交Spring AI tool calling例子

---
+++ springboot-ai-tool-calling/src/main/java/com/example/springbootaitoolcalling/SpringbootAiToolCallingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootaitoolcalling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAiToolCallingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAiToolCallingApplication.class, args);
+    }
+
+}
---
+++ springboot-ai-tool-calling/src/test/java/com/example/springbootaitoolcalling/SpringbootAiToolCallingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootaitoolcalling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAiToolCallingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
